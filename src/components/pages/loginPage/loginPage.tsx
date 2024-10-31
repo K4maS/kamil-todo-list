8e108dd23ef35e9dcb20e0eabf75ae652a5f3425
Kamil <107948849+K4maS@users.noreply.github.com>
2024-10-31 13:58:40 +0500

    Добавлена подсказка для логина

--- src/components/pages/loginPage/loginPage.tsx
+++ src/components/pages/loginPage/loginPage.tsx
@@ -12,7 +12,7 @@ export const LoginPage = () => {
 
   const navigate = useNavigate();
 
-  const { login, isAuth } = useUserStoreHooks();
+  const { login, isAuth, error } = useUserStoreHooks();
 
   const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -32,8 +32,8 @@ export const LoginPage = () => {
       <form className={styles.form} action='POST' onSubmit={onSubmit}>
         <Input type={'text'} placeholder='Логин' onInput={loginInput.onInput} value={loginInput.value} />
         <Input type={'password'} placeholder='Пароль' onInput={passwordInput.onInput} value={passwordInput.value} />
-
-        <Button text='Войти' type={'submit'} />
+                <Button text='Войти' type={'submit'} />
+        {error && <p className={styles.error}>{error}</p>}
       </form>
     </div>
   );
